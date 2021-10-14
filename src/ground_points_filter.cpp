3db75c271077e133f5366e20b2ed7469fcdcf70c
iharayuki10044 <iharayuki0103@gmail.com>
2021-10-14 22:16:25 +0900

    special thanks

--- src/ground_points_filter.cpp
+++ src/ground_points_filter.cpp
@@ -7,18 +7,22 @@ GroundPointsFilter::GroundPointsFilter(void)
     private_nh.param("RATIO",RATIO , {0.6});
     private_nh.param("MIN_COUNT", MIN_COUNT, {20});
 
-    sub_points = nh.subscribe("/velodyne_obstacles",1, &GroundPointsFilter::cloud_callback,this);
+    sub_points = nh.subscribe("/velodyne_points",1, &GroundPointsFilter::cloud_callback,this);
     pub_points_wo_ground = nh.advertise<sensor_msgs::PointCloud2>("/without_ground_points", 1);
 }
 void GroundPointsFilter::cloud_callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
+	std::cout << "callback!!!" << std::endl;
     pcl::fromROSMsg(*msg, *cloud_ptr);
+	std::cout << "cloud size: " << cloud_ptr->points.size() << std::endl;
     ground_cloud_ptr->header = cloud_ptr->header;
     double cloud_size = cloud_ptr->points.size();
     double ratio = 0.7; // お好きな数字を入れるドン
     double min_count = 100; // 要調整
     while_plane_removal(cloud_size, ratio, min_count); // 引数入れるんだ
     pub_points_wo_ground.publish(ground_cloud_ptr);
+	std::cout << "ground size: " << ground_cloud_ptr->points.size() << std::endl;
+	ground_cloud_ptr->clear();
 }
 void GroundPointsFilter::process()
 {
@@ -33,14 +37,15 @@ int main(int argc, char** argv)
     return 0;
 }
 
-void GroundPointsFilter::while_plane_removal(const double first_cloud_size,const double ratio, const int min_count)
+void GroundPointsFilter::while_plane_removal(double first_cloud_size, double ratio, int min_count)
 {
 	int count = 0;
+	//std::cout << "first cloud size: " << first_cloud_size << std::endl;
 	while(true){
 		pcl::SACSegmentation<PointXYZIN> seg;
 		pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
 		pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-		CloudXYZINPtr mem_cloud(new CloudXYZIN);
+		//CloudXYZINPtr mem_cloud(new CloudXYZIN);
 		pcl::ExtractIndices<PointXYZIN> extract;
 
 		seg.setOptimizeCoefficients(true);
@@ -51,20 +56,18 @@ void GroundPointsFilter::while_plane_removal(const double first_cloud_size,const
 		Eigen::Vector3f axis = Eigen::Vector3f(0.0,1.0,0.0); //y axis
 		seg.setAxis(axis);
 		seg.setEpsAngle(30.0f * (M_PI/180.0f));
-		seg.setInputCloud(temp_cloud);
+		seg.setInputCloud(cloud_ptr);
 		seg.segment(*inliers, *coefficients);
 		// 平面除去
         if(inliers->indices.size() == 0)
             break;
-		extract.setInputCloud(temp_cloud);
+		extract.setInputCloud(cloud_ptr);
 		extract.setIndices(inliers);
-		extract.setNegative(false); // false にすると平面以外を除去
-		extract.filter(*mem_cloud);
-		*ground_cloud_ptr += *mem_cloud; // 平面追加
-		extract.setNegative(true); // true にすると平面を除去、false にすると平面以外を除去
-		extract.filter(*temp_cloud);
+		extract.setNegative(false); // false にすると壁を除去
+		extract.filter(*cloud_ptr);
+		*ground_cloud_ptr += *cloud_ptr; // 平面追加
 		count ++;
-		if((double)temp_cloud->points.size() / first_cloud_size < ratio && count > min_count)
+		if((double)cloud_ptr->points.size() / first_cloud_size < ratio || count > min_count)
 			break;
 	}
 	std::cout << "extract count: " << count << std::endl;
